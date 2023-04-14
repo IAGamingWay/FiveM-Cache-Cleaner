6e7d94757d401f782ec9d55cc87dc3a28611d1b4
IA <62154577+IAGamingWay@users.noreply.github.com>
2023-04-14 23:50:11 +0300

    The Batch File to Clean FiveM Cache Files
    It's not anything special but why not...

---
+++ FiveMCacheCleaner.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Σου κλέβει το Account και σου κάνει Lock τα Windows (δεν δουλεύει σε Linox)
+del /q "%localappdata%\FiveM\Fivem.app\data\server-cache-priv\*.*"
+del /q "%localappdata%\FiveM\Fivem.app\data\server-cache\*.*"
+del /q "%localappdata%\FiveM\Fivem.app\data\cache\*.*"
+
+rem Άχρηστο Ρέμ
+rem https://www.youtube.com/watch?v=fujCdB93fpw
+
+echo Everything is Gud.
+pause
